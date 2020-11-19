ad921a6685c1ebe2289b5b615d976f7d1f56111a
Shouvick Halder <deeprony7@gmail.com>
2020-11-19 22:38:06 +0530

    added index and about pages

--- btre_project/pages/views.py
+++ btre_project/pages/views.py
@@ -1,6 +1,12 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render
+
 # Create your views here.
 
+
 def index(request):
-    return HttpResponse('<h1>Hello World</h1>')
+    return render(request, 'pages/index.html')
+
+
+def about(request):
+    return render(request, 'pages/about.html')
