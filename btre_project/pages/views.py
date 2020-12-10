fb57cf906c50e796e1d932de78d377446fafe456
Shouvick Halder <deeprony7@gmail.com>
2020-12-10 23:55:19 +0530

    working on home listings

--- btre_project/pages/views.py
+++ btre_project/pages/views.py
@@ -1,11 +1,18 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from listings.models import Listing
+
 # Create your views here.
 
 
 def index(request):
-    return render(request, 'pages/index.html')
+    listings = Listing.order_by('-listing_date').filter(is_published=True)[:3]
+
+    context = {
+        'listings': listings
+    }
+    return render(request, 'pages/index.html', context)
 
 
 def about(request):
