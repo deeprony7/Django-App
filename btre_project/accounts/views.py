ca9916cb6ad9e14a1a8a5dd556c87606ca3ec0ad
Shouvick Halder <deeprony7@gmail.com>
2020-12-25 23:25:17 +0530

    register and login templates

--- btre_project/accounts/views.py
+++ btre_project/accounts/views.py
@@ -2,11 +2,19 @@ from django.shortcuts import render, redirect
 
 
 def register(request):
-    return render(request, 'accounts/register.html')
+    if request.method == 'POST':
+        # register user
+        pass
+    else:
+        return render(request, 'accounts/register.html')
 
 
 def login(request):
-    return render(request, 'accounts/login.html')
+    if request.method == 'POST':
+        # login user
+        pass
+    else:
+        return render(request, 'accounts/login.html')
 
 
 def dashboard(request):
